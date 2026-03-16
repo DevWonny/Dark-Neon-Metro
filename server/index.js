2f995c8f369cdacfc75f3bd6a3bbfe2522c1092d
DevWonny <cjfdnjs1994@naver.com>
2026-03-16 15:13:47 +0900

    API 테스트 진행

---
+++ server/index.js
@@ -0,0 +1,64 @@
+// * Socket.io는 단독으로 실행되기 보다는 http 서버 위에서 동작
+// 웹 서버 프레임워크
+const express = require("express");
+// Node.js 기본 HTTP 모듈
+const http = require("http");
+const { Server } = require("socket.io");
+// 교차 출처 리소스 공유 보안 설정
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+// * 브라우저 보안 정책상 다른 도메인에서 해당 서버로 접근하는것을 허용해주는 설정
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    // * Next 주소
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const SUBWAY_KEY = process.env.SUBWAY_API_KEY;
+const NORMAL_KEY = process.env.NORMAL_API_KEY;
+const PORT = process.env.PORT;
+const URL = "http://swopenAPI.seoul.go.kr/api/subway";
+
+const getSubwayData = async (line) => {
+  try {
+    const url = encodeURI(
+      `${URL}/${SUBWAY_KEY}/json/realtimePosition/0/100/${line}`,
+    );
+
+    const response = await axios.get(url);
+    console.log("🚀 ~ getSubwayData ~ response:", response);
+
+    if (response.data) {
+      return response.data.realtimePositionList;
+    }
+    return null;
+  } catch (error) {
+    console.log("API 호출 에러", error);
+    return [];
+  }
+};
+
+app.get("/api/subway/:line", async (req, res) => {
+  const { line } = req.params;
+  console.log(`${line} 수신 요청`);
+
+  const data = await getSubwayData(line);
+  console.log("🚀 ~ data:", data);
+
+  if (data) {
+    res.json(data);
+  } else {
+    res.status(500).json({ error: "데이터를 가져오지 못했습니다." });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`포트 ${PORT} 작동중`);
+});
